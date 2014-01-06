64e755fee4ba4fc97b77f81f6a90f8ff74077db6
Valentin Hervieu <enforce46@hotmail.fr>
2014-01-06 10:53:24 +0100

    Amélioration de la position des GUI

--- Unity App/Assets/Scripts/GUIManager.cs
+++ Unity App/Assets/Scripts/GUIManager.cs
@@ -23,9 +23,10 @@ public class GUIManager : MonoBehaviour
     void Awake() {
         instance = this;
 		instance.comboLabel.text = "Combo: 0";
-		instance.multiplicateurlabel.text = "Multiplicateur: 1";
-		instance.pointsLabel.text = "Points: 0";
+		instance.multiplicateurlabel.text = "Bonus: x1";
+		instance.pointsLabel.text = "Score: 0";
 		instance.pourcentslabel.text = "100%";
+		instance.timeLabel.text = "loading...";
 		instance.debugLabel.enabled = false;
 		
 		//feedback management (adjust to screen)
@@ -34,22 +35,17 @@ public class GUIManager : MonoBehaviour
 		instance.screen.transform.localScale = new Vector3(Screen.width/100f, 1, Screen.height/100f);
 		
 		//adjust label size to screen
-		Debug.Log (Screen.width);
-		instance.timeLabel.fontSize = (int) Math.Round(Screen.width / 43d);
-		instance.comboLabel.fontSize = (int) Math.Round(Screen.width / 43d);
-		instance.multiplicateurlabel.fontSize = (int) Math.Round(Screen.width / 43d);;
-		instance.pointsLabel.fontSize = (int) Math.Round(Screen.width / 43d);
-		instance.pourcentslabel.fontSize = (int) Math.Round(Screen.width / 43d);
-		
-		//Adjust backgroud borders to screen
-		float factor = (float)((Screen.width + 39.8f)/3556f);
-		instance.GUIBorderLeft.guiTexture.transform.localScale = new Vector3(factor, factor , 1f);
-		instance.GUIBorderRight.guiTexture.transform.localScale = new Vector3(factor, factor , 1f);
+		instance.timeLabel.fontSize = (int) Math.Round(Screen.width / 45d);
+		instance.comboLabel.fontSize = (int) Math.Round(Screen.width / 45d);
+		instance.multiplicateurlabel.fontSize = (int) Math.Round(Screen.width / 45d);;
+		instance.pointsLabel.fontSize = (int) Math.Round(Screen.width / 45d);
+		instance.pourcentslabel.fontSize = (int) Math.Round(Screen.width / 45d);
+
     }
 	
 	public static void SetTime(float current, float total)
 	{
-		instance.timeLabel.text = String.Format("{0:00}:{1:00}",Math.Truncate(current/60f), Math.Truncate(current%60f))+" / "+String.Format("{0:00}:{1:00}",Math.Truncate(total/60f),Math.Truncate(total%60f));
+		instance.timeLabel.text = String.Format("{0:00}:{1:00}",Math.Truncate(current/60f), Math.Truncate(current%60f))+"/"+String.Format("{0:00}:{1:00}",Math.Truncate(total/60f),Math.Truncate(total%60f));
 	}
 	
 	public static void SetCountdown(int count)
@@ -63,7 +59,7 @@ public class GUIManager : MonoBehaviour
 	 
 	public static void SetPoints(int points)
 	{
-		instance.pointsLabel.text = "Points: "+points;
+		instance.pointsLabel.text = "Score: "+points;
 	}
 	 
 	public static void SetCombo(int combo)
